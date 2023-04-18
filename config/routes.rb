1be84b6da7a7399b563cddd0066ae9ae6f7ea210
Melaza6 <cueroleo6@gmail.com>
2023-04-18 12:02:02 -0700

    Initialize application structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :collections
   get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
-  root 'home/index'
+  root 'home#index'
   devise_for :users
 
 end
