6739a1f6320b2dc6030720660abd5c84da62fc5b
Melaza6 <cueroleo6@gmail.com>
2023-04-19 14:06:18 -0700

    add controller method for destroy

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -2,5 +2,10 @@ class CollectionsController < ApplicationController
     def index 
         collections = Collection.all 
         render json: collections
+    end 
+    def destroy
+        collections = Collection.find(parms[:id])
+        collections.destroy
+        render json: collections
     end
 end
