6bc032955b2695d0491cd74246a9f38fed07209a
CJ Norris <64042239+cmnorrisii@users.noreply.github.com>
2023-04-19 15:18:52 -0700

    Update collections_controller.rb

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -4,15 +4,6 @@ class CollectionsController < ApplicationController
         render json: collections
     end
 
-    def create
-        collection = Collection.create(collection_params)
-        if collection.valid?
-            render json: collection
-        else
-            render json: apartment.errors, status: 422
-        end
-    end
-
     def update
         collection = Collection.find(params[:id])
         collection.update(collection_params)
