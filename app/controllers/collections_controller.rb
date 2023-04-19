3efdfc27915f4104b70938a9c7ed337ab0411b5f
cmnorrisii <cmnorrisjr@gmail.com>
2023-04-19 14:25:06 -0700

    adds spec request testing and controller method for update

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -3,4 +3,28 @@ class CollectionsController < ApplicationController
         collections = Collection.all 
         render json: collections
     end
+
+    def create
+        collection = Collection.create(collection_params)
+        if collection.valid?
+            render json: collection
+        else
+            render json: apartment.errors, status: 422
+        end
+    end
+
+    def update
+        collection = Collection.find(params[:id])
+        collection.update(collection_params)
+        if collection.valid?
+            render json: collection
+        else
+            render json: collection.errors, status: 422
+        end
+    end
+
+    private
+    def collection_params
+        params.require(:collection).permit(:name, :category, :description, :condition, :image, :user_id)
+    end
 end
