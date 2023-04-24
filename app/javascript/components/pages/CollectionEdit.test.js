3dda6c658ec879d135f4296ffadb0b950862e1f2
mikerogers04 <mikerogers04@hotmail.com>
2023-04-24 09:42:53 -0700

    Adds additional testing for carousel attributes.

--- app/javascript/components/pages/CollectionEdit.test.js
+++ app/javascript/components/pages/CollectionEdit.test.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
+import fireEvent from '@testing-library/user-event'
 import CollectionEdit from "./CollectionEdit"
 import { MemoryRouter, Routes, Route } from "react-router-dom"
 import collections from "../mockCollections"
--- app/javascript/components/pages/ProtectedIndex.test.js
+++ app/javascript/components/pages/ProtectedIndex.test.js
@@ -35,5 +35,4 @@ describe("<ProtectedIndex />", () => {
     expect(collectionName).toBeInTheDocument()
     })
   })  
-
 })
