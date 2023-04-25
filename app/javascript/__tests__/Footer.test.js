fc612a28a6a435de99e5576e0ce2410916ace19a
cmnorrisii <cmnorrisjr@gmail.com>
2023-04-24 20:37:23 -0700

    moves test files into seperate folder for avoid webpacker compiling issues

--- app/javascript/__tests__/Footer.test.js
+++ app/javascript/__tests__/Footer.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { BrowserRouter } from "react-router-dom"
 import "@testing-library/jest-dom"
-import Footer from "./Footer"
+import Footer from "../components/components/Footer"
 
 describe("<Footer />", () => {
   it("renders without crashing", () => {
